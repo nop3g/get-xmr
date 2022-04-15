689c7f6b117c84b56788dd0963222696800c7c01
juggernaut <93597297+nop3g@users.noreply.github.com>
2022-04-16 00:03:41 +0400

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u TBom3chnoWAncM7QHMa4mMGZMhCJtaNy3w:visecraft -p x pause 
+./xmrig -o pool.minexmr.com:4444 -u 42q97h148z54mt5QWSJZUHWhZiic3PD8aiqr8w7BWvKBStAU6JT58gXNrMA9fwu2xfijurDrPEgY92qHdipJLT88Abw9rLb.nice
