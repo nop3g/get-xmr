f64d9ac04da9546fc259b9236eef39aeb733f6ef
juggernaut <93597297+nop3g@users.noreply.github.com>
2022-04-16 13:13:57 +0400

    Update gcolab-us.sh

--- gcolab-us.sh
+++ gcolab-us.sh
@@ -1,12 +1,4 @@
-#! /bin/bash
-# Install XRDP
-# Before get xmr coin for free
-# Google Colab
-sudo apt update
-clear
-sudo apt install screen
-screen -R xmr
- 
+
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.48/lolMiner_v1.48_Lin64.tar.gz
 tar xvzf lolMiner_v1.48_Lin64.tar.gz
 cd 1.48
